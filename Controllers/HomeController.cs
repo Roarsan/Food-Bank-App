d71b40498dc24448b79c25fae4cc53a32a936334
Arbaaz Patwari <arbaazpatwari@gmail.com>
2024-01-05 02:25:49 +0000

    adding about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CW1_MVC_Food_Bank_Website.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- obj/Debug/net8.0/CW1-MVC-Food-Bank-Website.AssemblyInfo.cs
+++ obj/Debug/net8.0/CW1-MVC-Food-Bank-Website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CW1-MVC-Food-Bank-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14cc25996137854fa6320f688265d4698838fb7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61a9f2d662e475dca6676d2f5aac947b133148b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CW1-MVC-Food-Bank-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CW1-MVC-Food-Bank-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
